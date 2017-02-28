b219b204bb2c63921c955d96ed45f5256ba75f6d
albioTQ <alberto.soragna@gmail.com>
2017-02-28 23:00:48 +0100

    Add files via upload
    Visualization finita

--- NLinkWithPlanner.m
+++ NLinkWithPlanner.m
@@ -10,21 +10,25 @@ I = 0.05;
 lc = 0.1; 
 
 active_joints = [0;1;0;1;1];
-
+n_joints = size(active_joints,1);
 
 %Planning parameters
-depthTree = 100;
-maxBranching = 6000;
+depthTree = 60;
+maxBranching = 3000;
 threshold = 0.05;
 deltaTPlanning = 0.15;
-deltaT = 0.005;
+deltaT = 0.015;
 for r = 0
-primitivesScaling = 1;
+primitivesScaling = 0.3;
 Knull = r; %This is used for projected gradient. Should not be a constant. When 0 projected gradient is disabled.
 
+primitives = [1, 0; -1, 0; 0, 1; 0, -1; 1, 1; 1, -1; -1, 1; -1,-1];
+primitives = primitives(: ,:);
+primitives = primitives*primitivesScaling;
+found = false;
 
 %Constraints on torques and joint positions
-tauLimit = 7;
+tauLimit = 2;
 jointLimitQ = pi/12;
 
 %Initial state and task goal state
@@ -51,38 +55,50 @@ end
 
 
 
-primitives = [1, 0; -1, 0; 0, 1; 0, -1; 1, 1; 1, -1; -1, 1; -1,-1];
-primitives = primitives(: ,:);
-primitives = primitives*primitivesScaling;
-found = false;
+
 
 graph = simplePlanning([q', qD'],goal, primitives,Knull, tauLimit, jointLimitQ, active_joints, depthTree, maxBranching, threshold, deltaTPlanning, deltaT);
 
-
+disp('after planning')
 %If I have not found a solution, plot something anyhow.
 if sum(strcmp(fieldnames(graph), 'solutionNode')) == 0
-   graph.solutionNode = size(graph.vectorEdges,2) -1 ;
+   graph.solutionNode = size(graph.vectorEdges,2)  ;
 else
    fname = strcat('solution_',mat2str(r), '.mat');
    found = true;
 end
+solutionID = graph.solutionNode;
+edgesIDList = graph.vectorEdges{solutionID}';
+nodeIterations = ceil(deltaTPlanning/deltaT);
 
-edgesIDList = graph.vectorEdges{graph.solutionNode}';
 
-n_joints = size(active_joints,1);
 stateStorage = zeros(size(edgesIDList,1), n_joints*2);
-actionsStorage = zeros(size(edgesIDList,1),2);
-taskState = zeros(2,1);
-taskStorage = zeros(size(edgesIDList,1), 2);
-errorStorage = zeros(size(edgesIDList,1), 2);
+actionsStorage = zeros(size(edgesIDList,1)-1,2);
+stateStorageNew = zeros(nodeIterations*size(actionsStorage,1) +1,n_joints*2);
+actionsStorageNew = zeros (nodeIterations*size(actionsStorage,1),2);
+taskStorage = zeros(size(stateStorageNew,1), 2);
+errorStorage = zeros(size(stateStorageNew,1), 1);
 
-for i=1:size(edgesIDList,1)
-    
-    stateStorage(i,:) = graph.verts(edgesIDList(i),:);
-    if (found == true)
-        save(fname, 'stateStorage');
+for i = 1:(size(edgesIDList,1)-1)
+    actionsStorage(i,:) = primitives(graph.actionsList{solutionID}(i),:);
+    actionsStorageNew(((i-1)*nodeIterations + 1): i*nodeIterations,:) = repmat(actionsStorage(i,:),nodeIterations,1);
+end
+
+for i = 1:(size(edgesIDList,1))
+    stateStorage(i,:) = graph.verts(edgesIDList(i),:);    
+end
+
+
+
+for i = 1:size(stateStorageNew,1)
+    if (i == 1)
+        stateStorageNew(i,:) =  [q',qD'];
+    else
+        stateStorageNew(i,:) = checkConstraints(stateStorageNew(i-1,:), actionsStorageNew(i-1,:)', r, deltaT,deltaT,tauLimit,jointLimitQ,active_joints,true);
     end
-    taskState = taskFunc(stateStorage(i,1),stateStorage(i,2),stateStorage(i,3),stateStorage(i,4),stateStorage(i,5));
+    
+    taskState = taskFunc(stateStorageNew(i,1),stateStorageNew(i,2),stateStorageNew(i,3),stateStorageNew(i,4),stateStorageNew(i,5));
+
     taskStorage(i,:) = taskState';
    
     currentAngleMat = [cos(taskState(1)), - sin(taskState(1)); sin(taskState(1)), cos(taskState(1))];
@@ -90,27 +106,34 @@ for i=1:size(edgesIDList,1)
     errorAngleMat = (currentAngleMat)\referenceAngleMat;
     errorAngleScalar = atan2(errorAngleMat(2,1),errorAngleMat(1,1));
             
-    errorStorage(i,:) = [errorAngleScalar, (taskState(2))- goal(2,1)];
+    errorStorage(i,:) = errorAngleScalar;
 
 end
 
-for i = 1:(size(edgesIDList,1)-1)
-    actionsStorage(i,:) = primitives(graph.actionsList{graph.solutionNode}(i));
-end
 
 
-timeStorage = 0:deltaTPlanning:(deltaTPlanning*(size(edgesIDList,1) - 1));
+
+timeStorage = 0:deltaT:(deltaT*(size(stateStorageNew,1) - 1));
+errorStorage = abs(errorStorage);
+figure
+plot(timeStorage,errorStorage(:,1)');
+title('Error Evolution')
+xlabel('Time')
+ylabel('Error')
 
 figure
-title('Error Evolution');
-xlabel('Time');
-ylabel('ERROR');
-subplot(2,1,1)
-plot(timeStorage,errorStorage(:,1)');
-subplot(2,1,2)
-plot(timeStorage,errorStorage(:,2)');
 
-%pause %Wait spacebar press to start real-time simulation 
+subplot(2,1,1)
+plot(timeStorage,taskStorage(:,1)');
+xlabel('Time')
+ylabel('Angle')
+title('Task 1 Evolution')
+subplot(2,1,2)
+plot(timeStorage,taskStorage(:,2)');
+xlabel('Time')
+ylabel('Length')
+title('Task 2 Evolution')
+pause %Wait spacebar press to start real-time simulation 
 
 figure
 figureLimits = l*5 + l/2;
@@ -150,13 +173,13 @@ end
 
 x0 = [0,0]; %Origin of the base link
 
-for i=1: size(stateStorage,1)
+for i=1: size(stateStorageNew,1)
 
-    x1 = [l*cos(stateStorage(i,1)), l*sin(stateStorage(i,1))];
-    x2 = x1 + [l*cos(stateStorage(i,1) + stateStorage(i,2)),l*sin(stateStorage(i,1) + stateStorage(i,2))];
-    x3 = x2 + [l*cos(stateStorage(i,1) + stateStorage(i,2) + stateStorage(i,3)),l*sin(stateStorage(i,1) + stateStorage(i,2)+ stateStorage(i,3))];
-    x4 = x3 + [l*cos(stateStorage(i,1) + stateStorage(i,2) + stateStorage(i,3) + stateStorage(i,4)),l*sin(stateStorage(i,1) + stateStorage(i,2)+ stateStorage(i,3)+ stateStorage(i,4))];
-    x5 = x4 + [l*cos(stateStorage(i,1) + stateStorage(i,2) + stateStorage(i,3) + stateStorage(i,4)+ stateStorage(i,5)),l*sin(stateStorage(i,1) + stateStorage(i,2)+ stateStorage(i,3)+ stateStorage(i,4)+ stateStorage(i,5))];
+    x1 = [l*cos(stateStorageNew(i,1)), l*sin(stateStorageNew(i,1))];
+    x2 = x1 + [l*cos(stateStorageNew(i,1) + stateStorageNew(i,2)),l*sin(stateStorageNew(i,1) + stateStorageNew(i,2))];
+    x3 = x2 + [l*cos(stateStorageNew(i,1) + stateStorageNew(i,2) + stateStorageNew(i,3)),l*sin(stateStorageNew(i,1) + stateStorageNew(i,2)+ stateStorageNew(i,3))];
+    x4 = x3 + [l*cos(stateStorageNew(i,1) + stateStorageNew(i,2) + stateStorageNew(i,3) + stateStorageNew(i,4)),l*sin(stateStorageNew(i,1) + stateStorageNew(i,2)+ stateStorageNew(i,3)+ stateStorageNew(i,4))];
+    x5 = x4 + [l*cos(stateStorageNew(i,1) + stateStorageNew(i,2) + stateStorageNew(i,3) + stateStorageNew(i,4)+ stateStorageNew(i,5)),l*sin(stateStorageNew(i,1) + stateStorageNew(i,2)+ stateStorageNew(i,3)+ stateStorageNew(i,4)+ stateStorageNew(i,5))];
 
     set(link1,'XData',[x0(1),x1(1)], 'YData',[x0(2),x1(2)])
     set(link2,'XData',[x1(1),x2(1)], 'YData',[x1(2),x2(2)])
@@ -165,12 +188,9 @@ for i=1: size(stateStorage,1)
     set(link5,'XData',[x4(1),x5(1)], 'YData',[x4(2),x5(2)])
 
     task_text.String = strcat(mat2str(double(vpa(taskStorage(i,1),3)),3),' COM Angle ',mat2str(double(vpa(taskStorage(i,2),3)),3),' COM Lenght');
-    time_text.String = mat2str(i*deltaTPlanning);
+    time_text.String = mat2str(i*deltaT);
 
     drawnow;
-
-    pause(deltaTPlanning);
-
 
 end
 
